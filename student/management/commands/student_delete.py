ed2dacb713c3b94d6dfa40e3c9c25f5a6ce67eca
Repala Swathi <swathi.repala@dhanushinfotech.net>
2020-09-28 15:01:25 +0530

    changes

--- student/management/commands/student_delete.py
+++ student/management/commands/student_delete.py
@@ -3,12 +3,12 @@ from django.utils.crypto import get_random_string
 from student.models import Student
 
 class Command(BaseCommand):
-    help = 'Closes the specified poll for voting'
+    help = 'delete specific student'
 
     def add_arguments(self, parser):
         parser.add_argument('student_id', nargs='+', type=int)
 
-    def handle(self, *args, **options):
+    def handle(self, **options):
         for student_id in options['student_id']:
             try:
                 student = Student.objects.get(pk=student_id).delete()
